2e35595557dc39cda754b06b71cbc08296235c03
Luis Sanchez <57106550+lsacera@users.noreply.github.com>
2023-12-12 17:39:52 +0100

    m4a to mp3

--- server.js
+++ server.js
@@ -176,7 +176,7 @@ function sendTwilioMessage(teneoResponse, res) {
 //TESTING: play music before hangup
     console.log("detected hangup, trying to play file...");
     twiml.play({
-    loop: 1}, 'https://demos-luis.artificial-solutions.com/artisoldemo/wait.m4a');
+    loop: 1}, 'https://demos-luis.artificial-solutions.com/artisoldemo/wait.mp3');
     console.log("... end playing file, trying to hangup");
 //TESTING: end
     response = twiml.hangup();
